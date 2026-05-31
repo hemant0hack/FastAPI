557096eda1dd66ba3531c3f9618359b5d528c3db
Hemant Rathore <hemant0hack@gmail.com>
2026-05-31 23:04:28 +0530

    update

--- Path_parameters/main.py
+++ Path_parameters/main.py
@@ -39,3 +39,21 @@ async def read_user():
 @app.get("/users")
 async def read_users2():
     return ["Bean", "Elfo"]
+
+# Predefineed value and create Enum class
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+# Path Converter
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
