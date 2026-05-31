9e0395c9b6ad131a8d823f0946a439254a9696c7
Hemant Rathore <hemant0hack@gmail.com>
2026-05-31 23:43:15 +0530

    update

--- Query_Parameters/main.py
+++ Query_Parameters/main.py
@@ -2,8 +2,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Query Parameters
+# URL http://127.0.0.1:8000/items/?skip=0&limit=10
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/")
+@app.get("/item/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+    
+# Optional parameters
+
+@app.get("/items/{items_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+# Query parameters type conversion
+@app.get("/itemss/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update(
+            {"descripation": "This is an amazing item that has a long description"}
+        )
+    return item
